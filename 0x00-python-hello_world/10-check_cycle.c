17ffa1d75975e817726fbd278f57f3e1c441e67e
Bensling <benslingwaruingi@gmail.com>
2023-08-07 07:42:50 -0700

    Python hello world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,23 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: pointer to the head of the list
- * Return: 1 if there is a cycle, 0 otherwise
+ * check_cycle - This checks if a linked list contains a cycle
+ * @list:This is a linked list to check
+ * Return: 1 if the list has a cycle, 0 if it does not
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (list == NULL || list->next == NULL)
+	if (!list)
 		return (0);
-
-	slow = list;
-	fast = list->next;
-
-	while (fast != NULL && fast->next != NULL)
+	while (slow && fast && fast->next)
 	{
-		if (slow == fast)
-			return (1);
 		slow = slow->next;
 		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
 	}
-
 	return (0);
 }
