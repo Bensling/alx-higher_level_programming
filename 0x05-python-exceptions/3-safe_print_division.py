2ace5e4c9b81b2beda12f0321cb18d58c09db60b
Bensling <benslingwaruingi@gmail.com>
2023-08-28 11:24:06 -0700

    updated

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         division = a / b
-    except (ZeroDivisionError, TypeError) :
+    except (ZeroDivisionError, TypeError):
         division = None
     finally:
         print("Inside result: {}".format(division))
