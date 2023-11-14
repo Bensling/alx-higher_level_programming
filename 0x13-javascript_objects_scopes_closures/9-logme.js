2bbd44cc8f425843fbbd33550dac043c0b93a9fb
Bensling <benslingwaruingi@gmail.com>
2023-11-14 08:30:36 -0800

    javascript_objects_scopes

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
-  const newlist = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    newlist.push(list[i]);
-  }
-  return newlist;
+
+let cuantity = 0;
+
+const logMe = (item) => {
+  console.log(`${cuantity}: ${item}`);
+  cuantity++;
 };
+
+module.exports = { logMe };
