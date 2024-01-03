5187f4ce0f4474a4ab5506d170333bfb8f167637
Bensling <benslingwaruingi@gmail.com>
2024-01-03 10:23:31 -0800

    js web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-// Extracting the file path and the string to write from the command line arguments
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-// Writing the string to the file in utf-8
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    // If an error occurred during writing, print the error object
-    console.error(err);
-  } else {
-    console.log('File written successfully');
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
